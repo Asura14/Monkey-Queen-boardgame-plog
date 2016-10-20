18b98f8f3ec60ecf708d72ff5e8e8e92ae4a0285
Luís Miguel Gonçalves <g.luismiguel14@gmail.com>
2016-10-20 11:05:49 +0100

    Board now has coordinates + prints :baby: with colour only

--- board.pl
+++ board.pl
@@ -39,15 +39,37 @@ initialBoard([	[0,0,0,0,0,b-20,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,w-20,0,0,0,0,0]]).
 
-printBoard([]):-
+printBoard([],Y):-
 	printLine(x).
 printBoard([H|T]):-
+	nl,
+	printTopCoor(x),
 	printLine(x),
+	write('1'),
+	write(' '),
 	printSpaces(H),
-	printBoard(T).
+	printBoard(T,1).
+printBoard([H|T],Y):-
+	Y < 9,
+	printLine(x),
+	Y1 is Y + 1,
+	write(Y1),
+	write(' '),
+	printSpaces(H),
+	printBoard(T,Y1).
+printBoard([H|T],Y):-
+	printLine(x),
+	Y1 is Y + 1,
+	write(Y1),
+	printSpaces(H),
+	printBoard(T,Y1).
+
+printTopCoor(X):-
+	write('    1   2   3   4   5   6   7   8   9  10  11  12'),
+	nl.
 
 printLine(X):-
-	write(' -----------------------------------------------'),
+	write('   -----------------------------------------------'),
 	nl.
 
 printSpaces([]):-
@@ -68,3 +90,7 @@ translatePrint(Colour-Char):-
 	Char >= 10,
 	write(Colour),
 	write(Char).
+translatePrint(X):-
+	write(' '),
+	write(X),
+	write(' ').
